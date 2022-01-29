d6d8845f8eb1607c18938c2e8823451cd39b8d20
Igor Toporkov <iigortoporkov@gmail.com>
2022-01-29 23:29:47 +0100

    Fix b52 converter; implement url shortening logic

--- app/service/Shortener.scala
+++ app/service/Shortener.scala
@@ -14,9 +14,19 @@ class Shortener @Inject() (urlMappingDao: UrlMappingDao) {
     urlMappingDao.getById(id)
   }
 
-
-  private def shortUrlToId(shortUrl: String) = {
+  def shortenUrl(longUrl: String): Future[String] = {
+    val r = urlMappingDao.getByLongUrl(longUrl)
+    r flatMap {
+      case Some(value) => Future { B52Converter.encode(value.id) }
+      case None => doShortenUrl(longUrl)
+    }
   }
+
+  private def doShortenUrl(longUrl: String): Future[String] = {
+    urlMappingDao.insert(longUrl) flatMap
+      (r => Future {B52Converter.encode(r.id)})
+  }
+}
 
   private object B52Converter {
 
@@ -24,22 +34,22 @@ class Shortener @Inject() (urlMappingDao: UrlMappingDao) {
     val alphabet: Seq[Char] = (('a' to 'z') ++ ('A' to 'Z')).toList
 
     def encode(long: Long): String = {
-      doEncode(long).mkString
+      doEncode(long).reverse.mkString
     }
 
     private def doEncode(long: Long): Seq[Char] = {
       if (long < base)
         alphabet(long.toInt) :: Nil
       else {
-        val rem = long / base
+        val rem = long % base
         val c = alphabet(rem.toInt)
-        c +: encode(long % base)
+        c +: doEncode(long / base)
       }
     }
 
-    def decode(string: String): Long =
+    def decode(string: String): Long = {
       string.toCharArray.foldRight((0L, 0))(
         (c, acc) => (acc._1 + alphabet.indexOf(c) * Math.pow(base, acc._2).toLong, acc._2 + 1)
       )._1 // TODO: replace indexOf
+    }
   }
-}
