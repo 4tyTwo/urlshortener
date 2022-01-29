eb88f57855fc876e4765edc329f855e4338ef68e
Igor Toporkov <iigortoporkov@gmail.com>
2022-01-29 01:07:07 +0100

    Create sample controller

--- app/controllers/shortenerController.scala
+++ app/controllers/shortenerController.scala
@@ -3,11 +3,20 @@ package controllers
 import javax.inject._
 import play.api.libs.json.Json
 import play.api.mvc._
+import service.Shortener
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
-class ShortenerController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class ShortenerController @Inject()(val controllerComponents: ControllerComponents, val shortener: Shortener) extends BaseController {
 
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(Json.toJson("Hello World"))
+  }
+
+  def redirect(shortUrl: String) = Action.async {
+    shortener.redirect(shortUrl) map {
+      case Some(value) => Ok(Json.toJson(value))
+      case None => Ok(Json.toJson("not found"))
+    }
   }
 }
