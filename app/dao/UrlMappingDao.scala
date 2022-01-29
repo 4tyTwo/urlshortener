ccc175d3586dde24e14a9b9f270fc3e2cbf76aca
Igor Toporkov <iigortoporkov@gmail.com>
2022-01-29 01:05:41 +0100

    Create dao for url mappings

---
+++ app/dao/UrlMappingDao.scala
@@ -0,0 +1,39 @@
+package dao
+
+import scala.concurrent.{ExecutionContext, Future}
+import javax.inject.Inject
+import models.UrlMapping
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+
+import java.sql.Timestamp
+
+class UrlMappingDao @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)(implicit executionContext: ExecutionContext) extends HasDatabaseConfigProvider[JdbcProfile] {
+  import profile.api._
+  private val urlMappings = TableQuery[UrlMappingTable]
+
+  def getByLongUrl(longUrl: String):Future[Option[UrlMapping]] = {
+    val q = urlMappings.filter(_.longUrl === longUrl)
+    db.run(q.result.headOption)
+  }
+
+  def insert(urlMapping: UrlMapping): Future[Int] = {
+    db.run(urlMappings += urlMapping)
+  }
+
+  def getById(id: Long): Future[Option[UrlMapping]] = {
+    val q = urlMappings.filter(_.id === id)
+    db.run(q.result.headOption)
+  }
+
+
+    private class UrlMappingTable(tag: Tag) extends Table[UrlMapping](tag, "url_mapping_v0") {
+
+    def longUrl = column[String]("long_url")
+    def id = column[Long]("id")
+    def created_at = column[Timestamp]("created_at")
+
+    def * = (id, longUrl, created_at) <> (UrlMapping.tupled, UrlMapping.unapply)
+  }
+}
