28b1754af0b72d3cc71ff6dd7b1eb10cb7bf185b
Igor Toporkov <iigortoporkov@gmail.com>
2022-02-13 18:56:58 +0100

    Refactor tests to match new layers

--- test/ShortenerServiceTest.scala
+++ test/ShortenerServiceTest.scala
@@ -1,60 +1,41 @@
-import dao.UrlMappingDaoComponent
+import repos.UrlMappingRepo
 import models.UrlMapping
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.MockitoSugar.when
 import org.scalatest.concurrent.ScalaFutures.whenReady
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
-import services.ShortenerServiceComponent
+import services.ShortenerServiceImpl
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-trait InsertNewTestEnv extends ShortenerServiceComponent with UrlMappingDaoComponent with MockitoSugar {
-  val mockDAO: UrlMappingDao = mock[UrlMappingDao]
-  when(mockDAO.insert(anyString())).thenReturn( Future {UrlMapping(1, "test", null)})
-  when(mockDAO.getByLongUrl(anyString())).thenReturn( Future { None })
-  override val urlMappingDao: UrlMappingDao = mockDAO
-  override val shortenerService: Shortener = new Shortener()
-}
-
-class ShortenerServiceTest1 extends PlaySpec with InsertNewTestEnv {
+class ShortenerServiceTest extends PlaySpec with MockitoSugar {
 
   "ShortenerService#create" should {
     "return a short url for a valid long one" in {
-      val actual = shortenerService.create("https://testurl")
+      val mockRepo: UrlMappingRepo = mock[UrlMappingRepo]
+      when(mockRepo.insert(anyString())).thenReturn( Future {UrlMapping(1, "test", null)})
+      when(mockRepo.getByLongUrl(anyString())).thenReturn( Future { None })
+      val actual = new ShortenerServiceImpl(mockRepo).create("https://testurl")
       whenReady(actual) { _ mustBe "b"}
     }
   }
-}
 
-trait InsertAgainTestEnv extends ShortenerServiceComponent with UrlMappingDaoComponent with MockitoSugar {
-  val mockDAO: UrlMappingDao = mock[UrlMappingDao]
-  when(mockDAO.getByLongUrl("https://testurl")).thenReturn( Future {Some(UrlMapping(1, "https://testurl", null))})
-  override val urlMappingDao: UrlMappingDao = mockDAO
-  override val shortenerService: Shortener = new Shortener()
-}
-
-class ShortenerServiceTest2 extends PlaySpec with InsertAgainTestEnv {
   "ShortenerService#create" should {
     "return the stored value if it is present" in {
-      val actual = shortenerService.create("https://testurl")
-      whenReady(actual) { _ mustBe "b"}
+      val mockRepo: UrlMappingRepo = mock[UrlMappingRepo]
+      when(mockRepo.getByLongUrl(anyString())).thenReturn( Future { Some { UrlMapping(2, "test", null) } })
+      val actual = new ShortenerServiceImpl(mockRepo).create("https://testurl")
+      whenReady(actual) { _ mustBe "c"}
     }
   }
-}
 
-trait LookUpTestEnv extends ShortenerServiceComponent with UrlMappingDaoComponent with MockitoSugar {
-  val mockDAO: UrlMappingDao = mock[UrlMappingDao]
-  when(mockDAO.getById(1)).thenReturn( Future {Some(UrlMapping(1, "https://testurl", null))})
-  override val urlMappingDao: UrlMappingDao = mockDAO
-  override val shortenerService: Shortener = new Shortener()
-}
-
-class ShortenerServiceTest3 extends PlaySpec with LookUpTestEnv {
   "ShortenerService#lookup" should {
     "return transform short url and perform lookup by id" in {
-      val actual = shortenerService.lookup("b")
+      val mockRepo: UrlMappingRepo = mock[UrlMappingRepo]
+      when(mockRepo.getById(1)).thenReturn( Future {Some(UrlMapping(1, "https://testurl", null))})
+      val actual = new ShortenerServiceImpl(mockRepo).lookup("b")
       whenReady(actual) { _ mustBe Some(UrlMapping(1, "https://testurl", null))}
     }
   }
